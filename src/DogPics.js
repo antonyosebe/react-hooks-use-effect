2e4acf70cc4cf1b46859cb46de7b29509cdca682
antonyosebe <osebe.anto@gmail.com>
2023-01-16 14:36:03 +0300

    completed assignment

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from "react";
-
-// WARNING: this useEffect will run in an infinite loop!
-// to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
-  const [images, setImages] = useState([]);
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
   useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
+    console.log("useEffect called");
   });
 
-  console.log("render");
+  console.log("Component rendering");
 
   return (
     <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
-      ))}
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
   );
 }
-
-export default DogPics;
